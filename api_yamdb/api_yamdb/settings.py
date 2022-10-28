0b98106ac7e9c37f0a9bbbf09f15063e51995803
Alexander Lonskiy <alexanderlonskiy@MacBook-Pro-Alexander.local>
2022-10-28 20:28:45 +0200

    this really shouldnt work

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -108,9 +108,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATIC_ROOT = (os.path.join(BASE_DIR, "static"))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media/'))
